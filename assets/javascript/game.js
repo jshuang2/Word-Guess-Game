bb399b8631c5ddfbcbba78226bdd42257aba7900
Jeff Huang <jeffshuang2@gmail.com>
2019-09-11 17:42:40 -0700

    added initial JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
 //Create an array of words
+var wordGuesses = ["apple", "seattle", "washington", "soccer", "groceries", "television", "computer", "university", "work", "mountain"]
 //Choose word randomly
+var randomNumber = Math.floor(Math.random() * wordGuesses.length);
+var chosenWord = randomNumber[wordGuesses];
+console.log(chosenWord);
 //Create underscores based on length of word
 //Get users guess
 //Check if guess is right
