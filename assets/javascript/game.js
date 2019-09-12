473fbeef42989d83bb77ba75d10d2fe5f7d2f6a4
Jeff Huang <jeffshuang2@gmail.com>
2019-09-11 17:08:12 -0700

    pseudocoded JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//Create an array of words
+//Choose word randomly
+//Create underscores based on length of word
+//Get users guess
+//Check if guess is right
+//If right, push to right array
+//If wrong, push to wrong array
